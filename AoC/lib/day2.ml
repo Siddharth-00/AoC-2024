0c0897352b994cb56e7d0249a45ef8a12dae8623
Siddharth Sharma <siddharthsharma@Siddharths-Air.broadband>
2024-12-02 22:14:55 +0000

    improved solution

--- AoC/lib/day2.ml
+++ AoC/lib/day2.ml
@@ -1,35 +1,26 @@
 open Core
 
-let rec diff_list = function
-  | x :: x' :: xs -> (x' - x) :: diff_list (x' :: xs)
-  | _ -> []
+let rec all_increasing_and_diff_in_range = function
+  | x :: x' :: xs ->
+      (x' > x && x' - x >= 1 && x' - x <= 3)
+      && all_increasing_and_diff_in_range (x' :: xs)
+  | [ _ ] | [] -> true
 
 let reports_safe =
   List.map ~f:(fun xs ->
-      let ds = diff_list xs in
-      let all_positive = List.for_all ds ~f:(fun n -> n > 0) in
-      let all_negative = List.for_all ds ~f:(fun n -> n < 0) in
-      let diff_in_range =
-        List.for_all ds ~f:(fun n -> 1 <= abs n && abs n <= 3)
-      in
-      (all_positive || all_negative) && diff_in_range)
+      let xs_rev = List.rev xs in
+      all_increasing_and_diff_in_range xs
+      || all_increasing_and_diff_in_range xs_rev)
 
-let rec get_lists_minus_one' xs ys =
-  match (xs, ys) with
-  | xs, y :: ys ->
-      List.append xs ys :: get_lists_minus_one' (List.append xs [ y ]) ys
-  | _, [] -> []
-
-let get_lists_minus_one = get_lists_minus_one' []
+let rec all_increasing_and_diff_in_range_with_one_error = function
+  | x :: x' :: xs ->
+      (x' > x && x' - x >= 1 && x' - x <= 3)
+      && all_increasing_and_diff_in_range_with_one_error (x' :: xs)
+      || all_increasing_and_diff_in_range (x' :: xs)
+  | [ _ ] | [] -> true
 
 let reports_safe_with_dampener =
   List.map ~f:(fun xs ->
-      let xxs_minus_one = get_lists_minus_one xs in
-      List.exists xxs_minus_one ~f:(fun xs_minus_one ->
-          let ds = diff_list xs_minus_one in
-          let all_positive = List.for_all ds ~f:(fun n -> n > 0) in
-          let all_negative = List.for_all ds ~f:(fun n -> n < 0) in
-          let diff_in_range =
-            List.for_all ds ~f:(fun n -> 1 <= abs n && abs n <= 3)
-          in
-          (all_positive || all_negative) && diff_in_range))
+      let xs_rev = List.rev xs in
+      all_increasing_and_diff_in_range_with_one_error xs
+      || all_increasing_and_diff_in_range_with_one_error xs_rev)
